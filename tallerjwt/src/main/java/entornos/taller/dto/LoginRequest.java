f6a6f17fbb4af6919a82424a0df9079e2b34757f
Silvia Cárdenas <silvia2210102@correo.uis.edu.co>
2025-04-09 23:49:03 -0500

    Fix LoginRequest

--- tallerjwt/src/main/java/entornos/taller/dto/LoginRequest.java
+++ tallerjwt/src/main/java/entornos/taller/dto/LoginRequest.java
@@ -4,19 +4,9 @@ public class LoginRequest {
     private String nombreUsuario;
     private String password;
 
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
+    public String getNombreUsuario() { return nombreUsuario; }
+    public void setNombreUsuario(String nombreUsuario) { this.nombreUsuario = nombreUsuario; }
 
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
